db3dd089f40e551fa7b471d6c849a8c432157f51
crawc <crawsol@gmail.com>
2023-09-30 12:09:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,14 +10,6 @@ for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
 done
 
 
-# Check if vim-addon installed, if not, install it automatically
-if hash vim-addon  2>/dev/null; then
-    echo "vim-addon (vim-scripts)  installed"
-else
-    echo "vim-addon (vim-scripts) not installed, installing"
-    sudo apt update && sudo apt -y install vim-scripts
-fi
-
 # Set the timezone to America/New_York
 sudo timedatectl set-timezone America/New_York
 
