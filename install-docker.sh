e28c362eaf64b85c969dfa8167065ab238f64f83
crawc <crawsol@gmail.com>
2024-03-07 15:18:37 -0500

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -9,14 +9,17 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 printf '\nDocker installed successfully\n\n'
 
+sudo usermod -aG docker $USER
+sudo -i -u $USER
+
 printf 'Waiting for Docker to start...\n\n'
 sleep 5
 
 # Docker Compose
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose"
-#sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 printf '\nDocker Compose installed successfully\n\n'
-sudo docker-compose -v
+docker-compose -v
+
